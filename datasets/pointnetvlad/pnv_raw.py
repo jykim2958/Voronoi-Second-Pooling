664154d9f2f8a9c0e525e813b4b4b447d3925883
qpwodlsqp <qpwodlsqp@gmail.com>
2026-03-16 01:26:25 +0900

    reproduce check & code factored

--- datasets/pointnetvlad/pnv_raw.py
+++ datasets/pointnetvlad/pnv_raw.py
@@ -1,6 +1,6 @@
 import numpy as np
 import os
-import open3d as o3d
+# import open3d as o3d
 
 from datasets.base_datasets import PointCloudLoader
 from pyntcloud import PyntCloud
@@ -28,10 +28,7 @@ class WildPlacesPointCloudLoader(PointCloudLoader):
         # Point clouds are already preprocessed with a ground plane removed
         self.remove_zero_points = False # True
         self.remove_ground_plane = False # True # NOTE Don't do this for the segmented
-        self.ground_plane_level = 0.5
-        # self.remove_zero_points = False
-        # self.remove_ground_plane = False
-        # self.ground_plane_level = None
+        self.ground_plane_level = None
 
     def read_pc(self, file_pathname: str) -> np.ndarray:
         # Reads the point cloud without pre-processing
@@ -41,38 +38,11 @@ class WildPlacesPointCloudLoader(PointCloudLoader):
         return pc 
 
     def __call__(self, file_pathname):
-        # Reads the point cloud from a disk and preprocess (optional removal of zero points and points on the ground
-        # plane and below
         # file_pathname: relative file path
         assert os.path.exists(file_pathname), f"Cannot open point cloud: {file_pathname}"
         pc = self.read_pc(file_pathname)
         pc = np.ascontiguousarray(pc)
         assert pc.shape[1] == 3
-
-        if self.remove_zero_points:
-            mask = np.all(np.isclose(pc, 0), axis=1)
-            pc = pc[~mask]
-
-        if self.remove_ground_plane:
-            pc = o3d.t.geometry.PointCloud(pc)
-            _, inliers = pc.segment_plane(distance_threshold=self.ground_plane_level,
-                                          ransac_n=3,
-                                          num_iterations=100)
-            pc = pc.select_by_index(inliers, invert=True)
-            pc = np.asarray(pc.to_legacy().points).astype(np.float32)
-
-        # # Downsample # NOTE don't do this for the segmented
-        # # if len(pc) > 4096:
-        # #     pc = o3d.t.geometry.PointCloud(pc)
-        # #     pc = pc.farthest_point_down_sample(4096)
-        # #     pc = np.asarray(pc.to_legacy().points).astype(np.float32)
-        # pc = o3d.t.geometry.PointCloud(pc)
-        # pc = pc.voxel_down_sample(voxel_size=0.1) # 0.1 0.5
-        # pc = np.asarray(pc.to_legacy().points).astype(np.float32)
-
-
-        # NOTE Variable Hieght Removal !!!
-        # pc = pc[pc[:, 2] <= 6]
 
         # NOTE Size normalization
         pc = pc / 30 # a max diameter should be around 60m # NOTE 60 30
